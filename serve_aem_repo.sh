11f6930f432bb6a7f400ee63f1e9e4652125c4b4
Filip Gołaś <filip.golas@3mdeb.com>
2025-02-06 16:05:58 +0100

    serve_aem_repo.sh: Add script to quickly setup testing AEM repo
    Signed-off-by: Filip Gołaś <filip.golas@3mdeb.com>

---
+++ serve_aem_repo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+REPO_DIR=$1
+
+key=$(gpg --batch --gen-key 2>&1 <<END
+Key-Type: 1
+Key-Length: 2048
+Subkey-Type: 1
+Subkey-Length: 2048
+Name-Real: AEM
+Expire-Date: 0
+%no-ask-passphrase
+%no-protection
+END
+)
+echo $key;
+
+if [[ $key =~ ([0-9A-Z]+)(\.rev) ]]; then
+    key_id="${BASH_REMATCH[1]}"
+fi
+
+gpg --export -a $key_id > $REPO_DIR/RPM-GPG-KEY-aem
+rpm --import RPM-GPG-KEY-aem
+rpm --define "_gpg_name $key_id" --addsign $REPO_DIR/rpm/*.rpm
+gpg --batch --yes --delete-secret-key $key_id
+gpg --batch --yes --delete-key $key_id
+
+createrepo_c $REPO_DIR
+cd $REPO_DIR && python3 -m http.server
